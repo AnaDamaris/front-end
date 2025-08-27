292427c1b15193490b56b7d14865a4bc72c389ea
Ana.D <ana.ciriaco.soares@escola.pr.gov.br>
2025-08-27 09:03:51 -0300

    Add files via upload

--- restaurante/lgpd.js
+++ restaurante/lgpd.js
@@ -1,7 +1,36 @@
+function doPost(e) {
+    var sheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
+    var data = e.parameter;
+    var email = data.email;
+   
+    // Obtém todos os emails existentes na planilha
+    var emails = sheet.getRange('B:B').getValues();
+   
+    // Verifica se o email já está na planilha
+    for (var i = 0; i < emails.length; i++) {
+      if (emails[i][0] === email) {
+        return ContentService.createTextOutput(JSON.stringify({"result":"error", "message":"O Email já registrado"}))
+                             .setMimeType(ContentService.MimeType.JSON);
+      }
+    }
+   
+    // Adiciona o novo registro se o email não estiver na planilha
+    sheet.appendRow([data.name, data.email, data.telefone]);
+   
+    return ContentService.createTextOutput(JSON.stringify({"result":"success"}))
+                         .setMimeType(ContentService.MimeType.JSON);
+  }
 
-document.getElementById('contactForm').addEventListener('submit', function(event) {
-if (!document.getElementById('dataConsent').checked) {
-event.preventDefault();
-alert('Você deve concordar com a Política de Privacidade e o uso de dados conforme a LGPD.');
-}
-});
+
+
+
+
+
+
+
+
+
+
+
+
+  
