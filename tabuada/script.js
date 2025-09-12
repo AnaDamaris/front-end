9d1514ddf8b002f9f15c9565f3f8319d5a2e11b3
Ana.D <ana.ciriaco.soares@escola.pr.gov.br>
2025-09-12 12:13:58 -0300

    Add files via upload

--- tabuada/script.js
+++ tabuada/script.js
@@ -1,18 +1,19 @@
-fuction calcularTabuada() {
-    let numero = document.getElementByI("numero").value;
+function calcularTabuada() {
+    let numero = document.getElementById("numero").value;
     let resultadoDiv = document.getElementById("resultado");
 
   if (numero === "") {
         resultadoDiv.innerHTML = "<p style='color:red'>Digite um número válido!</p>";
   } else {
-    let resultadoHTML = "<h3>Tabuada do " +
+    let resultadoHTML = "<h3>Tabuada do " + numero + "</h3>";
     let i = 1;
 
  while (i <= 10) {
-    resultadoHTML += "<p>" + numero + " x"
+    resultadoHTML += "<p>" + numero + " x " + i + " = " + (numero * i) + "</p>";
     i++;
  }
 
  resultadoDiv.innerHTML = resultadoHTML;
   }
+
 }
